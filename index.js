4605a6ed721da840f8b7589e837aeb734cea626f
Taibu Haroub <haroubtaibu@gmail.com>
2021-06-12 22:35:59 +0300

    Add check before doing an npm install

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require("express");
-const colors = require("colors");
-const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(morgan("dev"));
 
 app.get("/", (req, res, next) => {
   res.send("<h1>Server Up and Running</h1>")
 })
 
 app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`.yellow.bold);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
