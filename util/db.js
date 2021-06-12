d598b788ed53a20a3e73e3b008aee36083b94966
Taibu Haroub <haroubtaibu@gmail.com>
2021-06-13 01:53:10 +0300

    Set retry for db connection

--- util/db.js
+++ util/db.js
@@ -13,10 +13,16 @@ const connectDB = () => {
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+        useFindAndModify: false,
       }
     )
     .then(() => console.log("Sucessfully connected to DB"))
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      // setTimeout(connectDB, 5000); //will retry to connect to the mongodb sever until a connection is made
+      //by default monogdb trys connect for 30seconds and if it fails it throws an error
+      //adding the setTimeout will keep calling the function until a connection is made
+    });
 };
 
 module.exports = connectDB;
