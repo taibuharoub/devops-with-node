ecd18b77e83280b77a23735f33c76aa6bd16425d
Taibu Haroub <haroubtaibu@gmail.com>
2021-06-13 02:21:02 +0300

    Add error handling middleware

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get("/", (req, res, next) => {
   res.send("<h1>Server Up and Running</h1>");
 });
 
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
 });
