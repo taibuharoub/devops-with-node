16170911e205184e4798450029b063ecc46e4059
Taibu Haroub <haroubtaibu@gmail.com>
2021-06-13 01:15:38 +0300

    Set Envs for db

--- util/db.js
+++ util/db.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("../config/config");
 
 const connectDB = () => {
   mongoose
-    .connect("mongodb://root:toor@mongo/?authSource=admin", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     .then(() => console.log("Sucessfully connected to DB"))
     .catch((err) => console.log(err));
 };
