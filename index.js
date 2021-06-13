c8a603fd2e9c972e2056b942ad4ecf01e5a45912
Taibu Haroub <haroubtaibu@gmail.com>
2021-06-13 23:18:11 +0300

    Update session and redis configs

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require("express");
+const session = require("express-session");
+const redis = require("redis");
+let RedisStore = require("connect-redis")(session);
 
 const connectDB = require("./util/db");
-const red = require("./util/redis")
 
 connectDB();
 
 const postRouter = require("./routes/post");
 const authRouter = require("./routes/auth");
+const { REDIS_URL, REDIS_PORT, SESSION_SECRET } = require("./config/config");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(red); //sessions
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT,
+});
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 30000,
+    },
+  })
+);
 app.use(express.json({ limit: "1mb" }));
 
 app.get("/", (req, res, next) => {
--- util/redis.js
+++ util/redis.js
@@ -1,13 +1,29 @@
-const session = require("express-session")
-const redis = require("redis")
-const { REDIS_URL, REDIS_PORT, SESSION_SECRET } = require("../config/config")
-let RedisStore = require("connect-redis")(session)
+const session = require("express-session");
+const redis = require("redis");
+const { REDIS_URL, REDIS_PORT, SESSION_SECRET } = require("../config/config");
+let RedisStore = require("connect-redis")(session);
 let redisClient = redis.createClient({
-    host: REDIS_URL,
-    port: REDIS_PORT
-})
+  host: REDIS_URL,
+  port: REDIS_PORT,
+});
 
-const red = () => {
+module.exports = (req, res, next) => {
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 30000, //30 seconds
+    },
+  });
+
+  next();
+};
+
+/* const red = () => {
     session({
         store: new RedisStore({client: redisClient}),
         secret: SESSION_SECRET,
@@ -21,4 +37,4 @@ const red = () => {
     })
 }
 
-module.exports  = red
+module.exports  = red */
