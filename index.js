5bf186c7e8797b0b77a3c9d01c2e44e118b18559
Taibu Haroub <haroubtaibu@gmail.com>
2021-06-13 03:03:44 +0300

    Add routes for the post

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const connectDB = require("./util/db");
 
 connectDB();
 
+const postRouter = require("./routes/post");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res, next) => {
   res.send("<h1>Server Up and Running</h1>");
 });
+
+app.use("/api/v1/posts", postRouter);
 
 app.use((error, req, res, next) => {
   console.log(error);
